ecfe739a9d4a773f899442167ff24bc5fb3ed40c
Alex <fominsevil@gmail.com>
2025-11-24 21:03:17 +0300

    Fix: Update social icons SVG, contact info, add carousels for projects

--- src/components/ProjectsHome.tsx
+++ src/components/ProjectsHome.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "./ui/carousel";
 import img1 from "figma:asset/image2.png";
 import img2 from "figma:asset/image3.png";
 import img3 from "figma:asset/image4.png";
 
 export function ProjectsHome() {
+  const images = [img1, img2, img3];
+
   return (
     <section id="home" className="bg-white w-full py-12 md:py-20">
       <div className="w-full max-w-[1400px] xl:max-w-[1600px] 2xl:max-w-[1800px] mx-auto px-4 md:px-8">
@@ -21,17 +24,25 @@ export function ProjectsHome() {
           <p>мебели для HoReCa, офиса и дома</p>
         </div>
         
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
-          <div className="rounded-[10px] w-full h-[400px] md:h-[550px] lg:h-[650px] xl:h-[750px] overflow-hidden">
-            <img alt="Проект для дома 1" className="w-full h-full object-cover" src={img1} />
-          </div>
-          <div className="rounded-[10px] w-full h-[400px] md:h-[550px] lg:h-[650px] xl:h-[750px] overflow-hidden">
-            <img alt="Проект для дома 2" className="w-full h-full object-cover" src={img2} />
-          </div>
-          <div className="rounded-[10px] w-full h-[400px] md:h-[550px] lg:h-[650px] xl:h-[750px] overflow-hidden">
-            <img alt="Проект для дома 3" className="w-full h-full object-cover" src={img3} />
-          </div>
-        </div>
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          className="w-full"
+        >
+          <CarouselContent className="-ml-2 md:-ml-4">
+            {images.map((img, index) => (
+              <CarouselItem key={index} className="pl-2 md:pl-4 basis-full md:basis-1/3">
+                <div className="rounded-[10px] w-full h-[400px] md:h-[550px] lg:h-[650px] xl:h-[750px] overflow-hidden">
+                  <img alt={`Проект для дома ${index + 1}`} className="w-full h-full object-cover" src={img} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="left-4 md:left-8" />
+          <CarouselNext className="right-4 md:right-8" />
+        </Carousel>
       </div>
     </section>
   );
--- src/components/ProjectsHoreca.tsx
+++ src/components/ProjectsHoreca.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "./ui/carousel";
 import img1 from "figma:asset/9d277a0c76e94e6c0d1c36b260e7d33fa0e08d05.png";
 import imgImage5 from "figma:asset/77e65ee8bee14c18399a8e359e571f19624b33cc.png";
 import img3 from "figma:asset/c16e70d99e5d6998d8af8c71cbaf5a1f8b5f445c.png";
 
 export function ProjectsHoreca() {
+  const images = [img1, imgImage5, img3];
+
   return (
     <section id="horeca" className="bg-white w-full py-12 md:py-20">
       <div className="w-full max-w-[1400px] xl:max-w-[1600px] 2xl:max-w-[1800px] mx-auto px-4 md:px-8">
@@ -23,17 +26,25 @@ export function ProjectsHoreca() {
           <p>мебели для HoReCa, офиса и дома</p>
         </div>
         
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
-          <div className="rounded-[10px] w-full h-[400px] md:h-[550px] lg:h-[650px] xl:h-[750px] overflow-hidden">
-            <img alt="Проект HoReCa 1" className="w-full h-full object-cover" src={img1} />
-          </div>
-          <div className="rounded-[10px] w-full h-[400px] md:h-[550px] lg:h-[650px] xl:h-[750px] overflow-hidden">
-            <img alt="Проект HoReCa 2" className="w-full h-full object-cover" src={imgImage5} />
-          </div>
-          <div className="rounded-[10px] w-full h-[400px] md:h-[550px] lg:h-[650px] xl:h-[750px] overflow-hidden">
-            <img alt="Проект HoReCa 3" className="w-full h-full object-cover" src={img3} />
-          </div>
-        </div>
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          className="w-full"
+        >
+          <CarouselContent className="-ml-2 md:-ml-4">
+            {images.map((img, index) => (
+              <CarouselItem key={index} className="pl-2 md:pl-4 basis-full md:basis-1/3">
+                <div className="rounded-[10px] w-full h-[400px] md:h-[550px] lg:h-[650px] xl:h-[750px] overflow-hidden">
+                  <img alt={`Проект HoReCa ${index + 1}`} className="w-full h-full object-cover" src={img} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="left-4 md:left-8" />
+          <CarouselNext className="right-4 md:right-8" />
+        </Carousel>
       </div>
     </section>
   );
